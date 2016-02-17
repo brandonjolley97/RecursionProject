d068b57c96560f9f62169f67f965c9e2b31674d2
brandonjolley97 <brandonjolley97@gmail.com>
2016-02-17 09:04:57 -0700

    added basic methods and constructor for timer.

---
+++ src/model/CodeTimer.java
@@ -0,0 +1,29 @@
+package model;
+
+public class CodeTimer 
+{
+	
+	
+	private long executionTime;
+	
+	public CodeTimer()
+	{
+		this.executionTime = 0;
+	}
+	
+	public void startTimer()
+	{
+		this.executionTime = System.nanoTime();
+	}
+	
+	public void stopTimer()
+	{
+		this.executionTime = System.nanoTime() - executionTime;
+	}
+	
+	public void resetTimer()
+	{
+		this.executionTime = 0;
+	}
+	
+}
